4b219f24642637738ddc31f21ed302de3ece2ca3
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-11 12:29:08 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotoStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 2 Goto Statement
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 6)
+            {
+                goto end;
+            }
+                Console.WriteLine("i value: {0}", i);
+            }
+            end: Console.WriteLine("The end");
+            Console.ReadKey();
+        }
+    }
+}
